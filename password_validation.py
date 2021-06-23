894016e009af253764b4b67d1753f787466bee85
imran2t11 <imran2t11@gmail.com>
2021-06-23 23:08:32 +0600

    Update password_validation.py

--- password_validation.py
+++ password_validation.py
@@ -1,14 +1,14 @@
-def is_password_valid(password):
+def is_password_valid(passwd):
     # your code goes here
     # check the solution in the discord server.
-    if(len(password)<8):
+    if(len(passwd)<8):
         return False
-    numbers_count = sum(c.isdigit() for c in password)
+    numbers_count = sum(c.isdigit() for c in passwd)
     if(numbers_count<2): 
         return False
-    if not any(char.isupper() for char in password):  
+    if not any(char.isupper() for char in passwd):  
         return False
-    if not any(char.islower() for char in password): 
+    if not any(char.islower() for char in passwd): 
         return False
     else:
         return True
